5cebe80c8adad20c4affe5c5fd559481acb9b6aa
Yakser <iaksanov2044@mail.ru>
2020-01-03 22:06:21 +0400

    ITS WORKING!!!

--- bot.py
+++ bot.py
@@ -1,21 +1,40 @@
 import requests
+import json
 from time import sleep
 
 TOKEN = "991918669:AAGcmQcQYprrvUj-pSA2yZfBsGGTHQDBY5Y"
 MAIN_URL = f"https://api.telegram.org/bot{TOKEN}/"
 
 
+def get_data():
+    with requests.get("https://api.github.com/orgs/fedora-infra/repos") as r:
+        data = json.loads(r.text)
+    return data
+
+
+def get_forks_count():
+    output_message = ""
+    for x in get_data():
+        output_message += x["name"] + ": " + str(x["forks_count"]) + "\n"
+    return output_message
+
+
+def get_repo_forks(name):
+    for x in get_data():
+        if name in x["name"]:
+            return x["forks"]
+    return "Repository not found."
+
+
 def get_updates_json(request):
-    #   TODO
-    params = {'timeout': 100, 'offset': None}
-    response = requests.get(request + 'getUpdates')
+    params = {'timeout': 10, 'offset': None}
+    response = requests.get(request + 'getUpdates', data=params)
     return response.json()
 
 
 def last_update(data):
     results = data['result']
-    total_updates = [len(results) - 1]
-    return results[total_updates]
+    return results[len(results) - 1]
 
 
 def get_chat_id(update):
@@ -32,15 +51,13 @@ def main():
     update_id = last_update(get_updates_json(MAIN_URL))['update_id']
     while True:
         if update_id == last_update(get_updates_json(MAIN_URL))['update_id']:
-            send_message(get_chat_id(last_update(get_updates_json(MAIN_URL))), 'test')
+            last_chat_text = last_update(get_updates_json(MAIN_URL))['message']['text']
+
+            send_message(get_chat_id(last_update(get_updates_json(MAIN_URL))), get_repo_forks(last_chat_text))
+
             update_id += 1
         sleep(0.1)
 
 
-
 if __name__ == '__main__':
     main()
-
-"""
-chat_id = get_chat_id(last_update(get_updates_json(MAIN_URL)))
-send_message(chat_id, 'Your message goes here')"""
